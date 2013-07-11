03f0581ee167cc3f9e7dd1049bab58788bac3b82
Denis Krivostchokov <densomart@gmail.com>
2013-07-11 18:57:21 +0700

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pulm::Application.config.secret_token = 'd64786f163a20b6f4dd4685b2735345f353b5ba74fa78501d6ca8ec206d0c252f7feaa52fd3b315f92b289c93d48b20c6ce9e858abf7d54ce623114270eb0d82'
