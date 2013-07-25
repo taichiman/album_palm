13b6a058fb577d703f222279639a54cf94bdc500
Denis Krivostchokov <densomart@gmail.com>
2013-07-25 20:07:17 +0700

    add blorgh

---
+++ blorgh/config/routes.rb
@@ -0,0 +1,4 @@
+Blorgh::Engine.routes.draw do
+  resources :posts
+  root to: 'posts#index'
+end
---
+++ blorgh/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ae300380669c099f8fa97709df7f653bf427fae17506fca74d8da90bce4af7b62f04e0af7ff5598f6fd151b8c224ad520403493ab98908a6a87e87b16e456573'
